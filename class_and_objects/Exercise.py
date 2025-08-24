9ace38411de791aeb9df19529cdf336fcb0bd15e
Moises-Jorge <moisesjorge30@gmail.com>
2025-08-24 16:10:57 +0100

    update the exercise of classes & objects

--- class_and_objects/Exercise.py
+++ class_and_objects/Exercise.py
@@ -1,11 +1,14 @@
 class Pessoa:
-    def __init__(self, nome: str = '', idade: int = 0, profissao: str = ''):
-        self.nome = nome
+    def __init__(self, nome: str = '', idade: int = 0, profissao: str = '') -> None:
+        self.nome = nome.title()
         self.idade = idade
-        self.profissao = profissao
+        self.profissao = profissao.title()
 
-    def apresentar(self):
+    def apresentar(self) -> None:
         print(f'Ola, meu nome eh {self.nome}, tenho {self.idade} anos e sou {self.profissao}.')
+
+    def __str__(self) -> str:
+        return f'{self.nome}, {self.idade} anos, {self.profissao}'
 
 # Testando
 # Criando os objectos:
@@ -14,3 +17,5 @@ p2 = Pessoa("Margareth", 22, "Programadora")
 
 p1.apresentar()
 p2.apresentar()
+print(p1)
+print(p2)
